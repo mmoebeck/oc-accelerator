b89b8416f553fc0b2990c4cbc21a568396f4ec73
Erin <erincdustin@gmail.com>
2024-07-31 23:31:17 -0700

    remove redux

--- apps/storefront/src/hooks/useOcAuth.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { OcAuthState } from '../redux/ocAuth';
-import { useOcSelector } from '../redux/ocStore';
-
-const useOcAuth = (): OcAuthState => useOcSelector((s: any) => s.ocAuth);
-
-export default useOcAuth;
--- apps/storefront/src/hooks/useOcCurrentOrder.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { OcCurrentOrderState } from '../redux/ocCurrentOrder';
-import { useOcSelector } from '../redux/ocStore';
-
-const useOcCurrentOrder = (): OcCurrentOrderState => useOcSelector((s: any) => s.ocCurrentOrder);
-
-export default useOcCurrentOrder;
